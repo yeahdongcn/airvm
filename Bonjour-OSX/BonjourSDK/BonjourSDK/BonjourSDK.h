b4376de29e8e96ac4d1cb15966bae074ccc606d6
Jeff Wu <24978620@qq.com>
2016-06-18 10:29:54 +0800

    add sdk

---
+++ Bonjour-OSX/BonjourSDK/BonjourSDK/BonjourSDK.h
@@ -0,0 +1,11 @@
+//
+//  BonjourSDK.h
+//  BonjourSDK
+//
+//  Created by Sun Peng on 14-10-12.
+//  Copyright (c) 2014年 Peng Sun. All rights reserved.
+//
+
+#import <BonjourSDK/BSBonjourConnection.h>
+#import <BonjourSDK/BSBonjourServer.h>
+#import <BonjourSDK/BSBonjourClient.h>
