d0d091ba7e34273b02d7d573ee970cb3444f3ce0
Netiger <sadcup@qq.com>
2016-06-19 10:11:04 +0800

    Add SharedVM to hold a opened VM.

---
+++ AirVM/Bonjour-OSX/SharedVM.h
@@ -0,0 +1,15 @@
+//
+//  SharedVM.h
+//  AirVM
+//
+//  Created by Zhaokai Yuan on 6/19/16.
+//  Copyright © 2016 Peng Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SharedVM : NSObject
+
+@property (nonatomic, copy) NSString *vmName;
+
+@end
---
+++ AirVM/Bonjour-OSX/SharedVM.m
@@ -0,0 +1,13 @@
+//
+//  SharedVM.m
+//  AirVM
+//
+//  Created by Zhaokai Yuan on 6/19/16.
+//  Copyright © 2016 Peng Sun. All rights reserved.
+//
+
+#import "SharedVM.h"
+
+@implementation SharedVM
+
+@end
